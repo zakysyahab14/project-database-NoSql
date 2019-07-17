a01ee319819524ed162916534fc5867daf22687a
zakysyahab <zack4tress@gmail.com>
2019-07-17 18:05:36 +0700

    add underscore t mongo

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
-const MONGODB_CONNECTION = process.env.MONGODB_ATLAS || `mongodb://localhost/${process.env.MONGO_DATABASE}`
+const MONGODB_CONNECTION = process.env.MONGO_DB_ATLAS || `mongodb://localhost/${process.env.MONGO_DATABASE}`
  
 mongoose.connect(MONGODB_CONNECTION, {
     useNewUrlParser: true
 })
 
 .catch(error => {
-    console.log("error brow")
+    console.log("error at mongoose connection to database")
 })
 
 const db = mongoose.connection;
